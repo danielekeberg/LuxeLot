6312b524005df0976da4e43e144a95212963abaa
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-04-13 01:47:07 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const apiKey = 'c621a9e6-1acf-480f-81e1-8f3ec5f1337a';
+export const apiUrl = 'https://v2.api.noroff.dev';
+export const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGFuaWVsZWtlYmVyZyIsImVtYWlsIjoiZGFuZWtlNzUxNjBAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3NDQ0OTQyMDR9.trSh_Oto9_FEahwMuklpy3tHXPxOq3Qxu7TknSJRwyo';
+export const auth = localStorage.getItem('auth');
