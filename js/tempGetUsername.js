3fa55b7ae8accf88c813acdfe7d5f2064fb594f7
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-04-27 23:28:27 +0200

    Update tempGetUsername.js

--- js/tempGetUsername.js
+++ js/tempGetUsername.js
@@ -10,7 +10,4 @@ document.getElementById('username').addEventListener('click', () => {
     window.location.href = `./profile/?q=${tempUsername}`;
 });
 
-document.getElementById('profileInc').innerHTML = `
-<h1>hei ${tempUsername} :)</h1>
-<p>Denne siden er ikke ferdig, men den kommer snart!</p>
-<p>hehe</p>`;
+document.getElementById('pUsername').textContent = tempUsername;
