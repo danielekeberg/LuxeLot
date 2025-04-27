4e581bac86c2e38c5b9343a7924cbb6151c259d8
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-04-27 18:29:02 +0200

    Update listed.js

--- js/listed.js
+++ js/listed.js
@@ -5,7 +5,7 @@ const id = params.get('i');
 
 async function fetchListing() {
     try {
-        const response = await fetch(`${apiUrl}/auction/listings/${id}`, {
+        const response = await fetch(`${apiUrl}/auction/listings/${id}?_bids=true&_seller=true`, {
             headers: {
                 'Authorization': `Bearer ${auth}`,
             }
@@ -13,8 +13,12 @@ async function fetchListing() {
         const data = await response.json();
         console.log(data);
         document.getElementById('listed').innerHTML = `
-        <h1>${data.data.title}</h1>
-        <img src="${data.data.media[0].url}">
+        <img src="${data.data.media[0].url};">
+        <div class="">
+            <h1>${data.data.name}</h1>
+            <p>${data.data._count.bids}</p>
+        </div>
+        <p id="timeLeft">0</p>
         `;
     } catch(err) {
         console.error(err);
@@ -22,3 +26,27 @@ async function fetchListing() {
 }
 
 fetchListing();
+
+async function timeLeft() {
+    try {
+        const response = await fetch(`${apiUrl}/auction/listings/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${auth}`,
+            }
+        });
+        const data = await response.json();
+        const endTime = new Date(data.data.endsAt);
+        const now = new Date();
+        const diffsInMs = endTime - now;
+        const totalSeconds = Math.floor(diffsInMs / 1000);
+        const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, '0');
+        const minutes = String(Math.floor(totalSeconds / 3600) / 60).padStart(2, '0');
+        const seconds = String(totalSeconds % 60).padStart(2, '0');
+        document.getElementById('timeLeft').textContent = `${hours}:${minutes}:${seconds}`
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+// timeLeft();
+// setInterval(timeLeft, 1000)
