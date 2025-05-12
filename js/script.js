deefb6a62f65f8d578b273b380a08c79f0b3c797
Daniel Lehre Ekeberg <danieleke@live.no>
2025-05-12 03:15:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { apiUrl, apiKey, isLoggedIn, auth } from './config.js';
+import { isLoggedIn, apiUrl, apiKey, auth, username } from './config.js';
 
 isLoggedIn();
 
@@ -8,3 +8,33 @@ function getUsername() {
 }
 
 getUsername();
+
+document.querySelector('.create').title = 'Create an auction!';
+
+async function hoverTitle() {
+    try {
+        const response = await fetch(`${apiUrl}/auction/profiles/${username}`, {
+            headers: {
+                'Authorization': `Bearer ${auth}`,
+                'X-Noroff-API-Key': `${apiKey}`,
+            }
+        });
+        const data = await response.json();
+        document.querySelector('.credits').title = `${username} has ${data.data.credits} credits!`;
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+hoverTitle();
+
+function search() {
+    const searchInput = document.getElementById('searchInput').value;
+    window.location.href = `../search/?q=${searchInput}`;
+}
+
+document.getElementById('searchInput').addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        search();
+    }
+})
